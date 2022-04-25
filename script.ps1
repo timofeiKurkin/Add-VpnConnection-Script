f2987fb7698cdcf08cb5e44a39e0cf77826a3ee8
timofey <ydepression83@gmail.com>
2022-04-25 22:00:17 +0700

    rename 2

--- script.ps1
+++ script.ps1
@@ -1,8 +1,8 @@
 ﻿$VpnName = "Test VPN"
-$gateway = "8.8.8.8"
+$serverAddress = "8.8.8.8"
 $psk = "4"
 $tunnelType = "L2tp"
 $encryptionLevel="Required"
 $authMethod = "MSChapv2"
 
-Add-VpnConnection -Name $VpnName -ServerAddress $gateway -TunnelType $tunnelType -EncryptionLevel $encryptionLevel -AuthenticationMethod $authMethod -L2tpPsk $psk -Force -AllUserConnection -RememberCredential -SplitTunneling -PassThru
+Add-VpnConnection -Name $VpnName -ServerAddress $serverAddress -TunnelType $tunnelType -EncryptionLevel $encryptionLevel -AuthenticationMethod $authMethod -L2tpPsk $psk -Force -AllUserConnection -RememberCredential -SplitTunneling -PassThru
