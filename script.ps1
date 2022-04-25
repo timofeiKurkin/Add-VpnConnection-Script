d36d6a14ccb72ea4570a0b80b699a4bc9fdbdd00
timofey <ydepression83@gmail.com>
2022-04-25 21:55:32 +0700

    create script

---
+++ script.ps1
@@ -0,0 +1,8 @@
+﻿$VpnName = "Test VPN"
+$gateway = "8.8.8.8"
+$psk = "123456"
+$tunnelType = "L2tp"
+$encryptionLevel="Required"
+$authMethod = "MSChapv2"
+
+Add-VpnConnection -Name $VpnName -ServerAddress $gateway -TunnelType $tunnelType -EncryptionLevel $encryptionLevel -AuthenticationMethod $authMethod -L2tpPsk $psk -Force -AllUserConnection -RememberCredential -SplitTunneling -PassThru
